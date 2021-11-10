eed48721aa3a57b43118d9719960d368dc2db26a
AnaSavu <anasavu001@>
2021-11-10 14:54:08 +0200

    implemented lab 4

--- FiniteAutomata.py
+++ FiniteAutomata.py
@@ -1,5 +1,7 @@
 #E - epsilon
 #S - sigma
+import itertools
+
 
 class FA:
     def __init__(self, Q, E, S, q0, F):
@@ -9,13 +11,45 @@ class FA:
         self.q0 = q0
         self.F = F
 
-    def parseLine(self):
-        pass
-
+    # return the set of states
     def getQ(self):
         return self.Q
 
+    # return the alphabet
+    def getE(self):
+        return self.E
+
+    # return all the transitions
+    def getS(self):
+        return self.S
+
+    # return the set of final states
+    def getF(self):
+        return self.F
+
+    def isDFA(self):
+        for a, b in itertools.combinations(self.S, 2):
+            if a[0] == b[0]:
+                raise Exception("It is not DFA!")
+            # print(a[0], end="")
+            # print(b[0])
+            # compare(a[0], b[0])
+
+    def isSequenceAcceptedByFA(self, sequence):
+        currentState = self.q0
+
+        for i in range(len(sequence)):
+            posi = sequence[i]
+            ok = 0
+            for transition in self.S:
+                if transition[0][1] == posi and transition[0][0] == currentState:
+                    currentState = transition[1]
+                    ok = 1
+                    break
+            if ok == 0:
+                raise Exception("Sequence is not accepted by the FA!")
+
     def toString(self):
-        return "Q: " + str(self.Q) + "\n" + "E: " + str(self.E) + "\n" + "S: " + self.S + "\n" + "q0: " + self.q0 + "\n" + "F: " + str(self.F)
+        return "Q: " + str(self.Q) + "\n" + "E: " + str(self.E) + "\n" + "S: " + str(self.S) + "\n" + "q0: " + str(self.q0) + "\n" + "F: " + str(self.F)
 
 
--- main.py
+++ main.py
@@ -1,37 +1,106 @@
 from FiniteAutomata import *
 
-def parseTransitions(parts):
+def determineTransitions(parts, Q, E):
     result = []
     transitions = []
     index = 0
 
     while index < len(parts):
-        transitions.append(parts[index] + ',' + parts[index + 1])
-        index += 2
+        transitions.append(parts[index])
+        index += 1
 
     for transition in transitions:
         lhs, rhs = transition.split('->')
         state2 = rhs.strip()
+
+        if state2 not in Q:
+            raise Exception(state2 + " is not in the set of states!")
+
         state1, route = [value.strip() for value in lhs.strip()[1:-1].split(',')]
+
+        if state1 not in Q:
+            raise Exception(state1 + " is not in the set of states!")
+        if route not in E:
+            raise Exception(route + " is not defined in the alphabet!")
 
         result.append(((state1, route), state2))
 
     return result
 
-def parseLine(line):
-    return [value.strip() for value in line.split('=')[1].strip()[1:-1].strip().split(',')]
+def transformLine(line):
+    return [value.strip() for value in line.split('=')[1].strip()[1:-1].strip().split(';')]
 
-def readFromFile(fileName):
+def readFromFileAndVerify(fileName):
     with open(fileName) as file:
-        Q = parseLine(file.readline())
-        E = parseLine(file.readline())
-        print(''.join([line for line in file]))
-        #S = parseTransitions(FA.parseLine(''.join([line for line in file])))
+        Q = transformLine(file.readline())
+        E = transformLine(file.readline())
+        try:
+            S = determineTransitions(transformLine(file.readline()), Q, E)
+        except Exception as e:
+            print(e)
+        # S = parseTransitions(prseLine(''.join([line for line in file])))
         q0 = file.readline().split('=')[1].strip()
-        F = parseLine(file.readline())
+
+        if len(q0) != 1:
+            raise Exception("There are more the one initial state!")
+        if q0 not in Q:
+            raise Exception("The initial state is not in the set of states!")
+
+        F = transformLine(file.readline())
+
+        for state in F:
+            if state not in Q:
+                raise Exception("State " + state + " is not in the set of states!")
 
         return FA(Q, E, S, q0, F)
 
+def printMenu():
+    print("Menu:")
+    print("1. the set of states")
+    print("2. the alphabet")
+    print("3. all the transitions")
+    print("4. the set of final states")
+
 if __name__ == "__main__":
-    FA = readFromFile("fa.in")
-    print(FA.toString())
+    try:
+        FA = readFromFileAndVerify('fa.in')
+        print("It is a valid FA!")
+    except Exception as e:
+        print(e)
+
+    try:
+        FA.isDFA()
+        print("It is DFA!")
+    except Exception as e:
+        print(e)
+
+    print('sequence: ', end="")
+    sequence = input()
+
+    try:
+        FA.isSequenceAcceptedByFA(sequence)
+        print("Sequence is accepted by the FA!")
+    except Exception as e:
+        print(e)
+
+    while(True):
+        print()
+        printMenu()
+        print('>>> ', end="")
+        try:
+            command = int(input())
+            print()
+            if command == 1:
+                print(FA.getQ())
+            elif command == 2:
+                print(FA.getE())
+            elif command == 3:
+                print(FA.getS())
+            elif command == 4:
+                print(FA.getF())
+            else:
+                print("Wrong command! Try again")
+        except ValueError as e:
+            print(e)
+
+    # print(FA.toString())
